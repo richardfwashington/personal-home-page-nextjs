0034cc3ff10148a77a4cadee85f63cca4e921180
Richard Washington <richard.washington@taxassist.co.uk>
2024-02-15 14:41:48 +0000

    :sparkles: Upgade to Next.js 14

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
-    formats: ['image/avif', 'image/webp'],
-  }
-}
+    formats: ["image/avif", "image/webp"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
