f914c03825548756f9f4d6494a571a7d90228ce3
Richard Washington <richard.washington@taxassist.co.uk>
2024-02-15 14:06:14 +0000

    :zap: Add vercel performance tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Image from "next/image";
 import profilePic from "../../public/KenshinKyu_big_brain_nick_e39a2fe1-19b3-4c27-b06d-9581284e54bd.png";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "Something weird",
